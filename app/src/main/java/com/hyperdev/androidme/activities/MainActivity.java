67f21e11910f19babd4c613904c0aa94733e4525
jairaCabarubias <jairagrace16@gmail.com>
2017-09-08 20:38:50 +0800

    Final Commit

--- app/src/main/java/com/hyperdev/androidme/activities/MainActivity.java
+++ app/src/main/java/com/hyperdev/androidme/activities/MainActivity.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.hyperdev.androidme.R;
+import com.hyperdev.androidme.data.BodyPartFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    public BodyPartFragment mBodyPartsFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.mFragmentLayout, new BodyPartFragment()).commit();
     }
 }
--- app/src/main/java/com/hyperdev/androidme/data/BodyPartFragment.java
+++ app/src/main/java/com/hyperdev/androidme/data/BodyPartFragment.java
@@ -1,19 +1,28 @@
 package com.hyperdev.androidme.data;
 
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.hyperdev.androidme.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BodyPartFragment extends Fragment {
 
+    private ImageView mImgHead, mImgBody, mImgLegs;
+    private List<Integer> mHeadList, mBodyList, mLegList;
+    String BodyParts;
 
     public BodyPartFragment() {
         // Required empty public constructor
@@ -24,7 +33,54 @@ public class BodyPartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_body_part, container, false);
+        View view = inflater.inflate(R.layout.fragment_body_part, container, false);
+
+        mImgHead = (ImageView) view.findViewById(R.id.Viewhead);
+        mImgBody = (ImageView) view.findViewById(R.id.Viewbody);
+        mImgLegs = (ImageView) view.findViewById(R.id.Viewleg);
+
+        mHeadList = new ArrayList<>();
+        mBodyList = new ArrayList<>();
+        mLegList = new ArrayList<>();
+
+        mHeadList = AndroidImageAssets.getHeads();
+        mBodyList = AndroidImageAssets.getBodies();
+        mLegList = AndroidImageAssets.getLegs();
+
+        final Random random = new Random();
+
+
+
+        mImgHead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int numHead = random.nextInt(mHeadList.size()-1);
+                Drawable headDrawable = getResources().getDrawable(mHeadList.get(numHead));
+                mImgHead.setImageDrawable(headDrawable);
+            }
+        });
+
+        mImgBody.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int numBody = random.nextInt(mBodyList.size()-1);
+                Drawable bodyDrawable = getResources().getDrawable(mBodyList.get(numBody));
+                mImgBody.setImageDrawable(bodyDrawable);
+
+            }
+        });
+
+        mImgLegs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final int numLegs = random.nextInt(mLegList.size()-1);
+                Drawable legsDrawable = getResources().getDrawable(mLegList.get(numLegs));
+                mImgLegs.setImageDrawable(legsDrawable);
+            }
+        });
+
+        return view;
+
     }
 
 }
